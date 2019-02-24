28fb15e13804f71a89c9fbaa7c26073ff7aae69c
Iliyan Videv <videviliyan@gmail.com>
2019-02-24 10:03:23 +0200

    Add an app icon

--- setup.py
+++ setup.py
@@ -1,3 +1,13 @@
 #!/usr/local/bin/python3
 
 from distutils.core import setup
+
+setup(name='CWG',
+      version='1.0',
+      description='A casual word guessing game.',
+      author='Iliyan Videv',
+      author_email='videviliyan@gmail.com',
+      url='https://github.com/ivi-dev/CWG/',
+      packages=['src'],
+      package_data={'': ['data/*.*']},
+      )
