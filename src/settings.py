826c8fa22e3707a7317d33e029bef86e058776ea
Iliyan Videv <videviliyan@gmail.com>
2019-02-27 11:19:08 +0200

    Updated game version in settings

--- src/settings.py
+++ src/settings.py
@@ -5,7 +5,7 @@ class Settings:
 
     programName = 'Casual Word Game'
 
-    programVersion = '1.1.0'
+    programVersion = '1.2.0'
 
     # Indicates how many letters to reveal from a word (0.5 means half of the letters, 0.3 one third of the letters
     # etc.)
