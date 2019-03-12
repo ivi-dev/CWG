551d92117ebc5a3c5af2cbefc8046585977a159a
Iliyan Videv <videviliyan@gmail.com>
2019-03-12 16:49:14 +0200

    Add a settings window.

--- src/content.py
+++ src/content.py
@@ -1,8 +1,16 @@
 # The text for the labels in the info area
 infoLabels = [('ATTEMPTS', 'attempts'), ('SYNONYMS', 'synonyms'), ('PART OF SPEECH', 'partOfSpeech')]
 
+# The text for the labels in the settings
+settingsLabels = [('DIFFICULTY', 'settings_difficulty', 'difficulty'),
+                  ('LANGUAGE', 'settings_language', 'language'), ('MUSIC', 'settings_music', 'music'),
+                  ('SOUND FX', 'settings_sound_fx', 'sfx')]
+
 # The labels of the buttons. These hint to the code what image filename to choose for each button
 buttonLabels = ['skip', '+a', 'settings', 'exit']
+
+# The labels of the buttons. These hint to the code what image filename to choose for each button
+settingsButtonLabels = ['apply', 'back']
 
 # A collection of images from the UI. Keeps references to make sure that images are displayed
 images = []
@@ -15,6 +23,12 @@ word = {}
 
 # Handles to canvas elements
 canvasElements = {
-    'letters': []
+    'wordDef': None,
+    'letters': [],
+    'attempts': None,
+    'synonyms': None,
+    'partOfSpeech': None,
+    'time': None,
+    'settingsLabels': []
 }
 
--- src/gui_settings.py
+++ src/gui_settings.py
@@ -1,3 +1,6 @@
+# The settings file
+settingsFile = 'src/data/settings.json'
+
 # The program's name
 programName = 'Casual Word Game'
 
@@ -6,15 +9,31 @@ programVersion = '2.0'
 
 # Indicates how many letters to reveal from a word (0.5 means half of the letters, 0.3 one third of the letters
 # etc.). Varies with the 'difficulty'
-revealLettersRatio = {'easy': 0.5, 'medium': 0.5, 'hard': 0.3}
+revealLettersRatio = {'Easy': 0.5, 'Medium': 0.5, 'Hard': 0.3}
 
-# Controls the game's difficulty. Could be either 'easy', 'medium' (the default) or 'hard'
-difficulty = 'medium'
+# Controls the game's difficulty. Could be either 'Easy', 'Medium' (the default) or 'Hard'
+difficulty = 'Medium'
+difficultyOptions = ['Easy', 'Medium', 'Hard']
+
+# The display name of the player
+displayName = 'Player'
+
+# The language of the game
+language = 'EN'
+languageOptions = ['En', 'Bg', 'De']
+
+# Music on (True) or off (False)
+music = 'On'
+musicOptions = ['On', 'Off']
+
+# SFX on (True) or off (False)
+sfx = 'On'
+sfxOptions = ['On', 'Off']
 
 # Indicates the number of attempts to guess the word. The numbers mean 'add that many to the value of
-# revealLettersRatio'. Example: On the 'easy' setting if there are 3 letters revealed, one would have 5 (3 + 2)
+# revealLettersRatio'. Example: On the 'Easy' setting if there are 3 letters revealed, one would have 5 (3 + 2)
 # attempts to guess the word. Varies with the 'difficulty'
-attempts = {'easy': 2, 'medium': 1, 'hard': 0}
+attempts = {'Easy': 2, 'Medium': 1, 'Hard': 0}
 
 # Indicates the time available to guess the word. Varies with the 'difficulty'. Expressed in seconds
-time = {'easy': 181, 'medium': 121, 'hard': 61}
+time = {'Easy': 181, 'Medium': 121, 'Hard': 61}
